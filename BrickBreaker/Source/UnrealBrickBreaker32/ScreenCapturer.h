16e308514225d1ba12d64adbb8b068975da2dda6
bertall <albert.baishev@yandex.ru>
2016-12-27 15:19:58 +0300

    BrickBreaker project

---
+++ BrickBreaker/Source/UnrealBrickBreaker32/ScreenCapturer.h
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "ScreenCapturer.generated.h"
+
+UCLASS()
+class UNREALBRICKBREAKER32_API AScreenCapturer : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this actor's properties
+    AScreenCapturer();
+
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    // Called every frame
+    virtual void Tick( float DeltaSeconds ) override;
+
+    UPROPERTY(EditAnywhere, Category = Screenshot)
+    TArray<uint8> Screenshot;
+
+    UPROPERTY(EditAnywhere, Category = Screenshot)
+    int Height;
+
+    UPROPERTY(EditAnywhere, Category = Screenshot)
+    int Width;
+
+    UPROPERTY(EditAnywhere, Category = Screenshot)
+    float ScreenshotPeriod;
+
+private:
+    bool CaptureScreenshot(TArray<uint8>* data);
+
+    float ScreenshotTimer;
+
+};
